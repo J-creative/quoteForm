f9c7117cfbbf51a5f05c70a5fe09510486cbe89d
Jacob Turner <jacobturnr@gmail.com>
2021-05-09 16:13:00 +0100

    starting to recreate wordpress homepage in react

--- src/App.js
+++ src/App.js
@@ -1,80 +1,12 @@
-
-import './App.css';
-import './css/switch.css';
-import './css/table.css';
-import './css/extras.css';
-import './css/formats.css';
-import React, { useState } from 'react'
-import Intro from './components/Intro';
-import HeroShot from './components/HeroShot';
-import CreateScene from './components/CreateScene';
-import Extras from './components/Extras';
-import Pricecalc from './components/Pricecalc';
-import Formats from './components/Formats';
-import Total from './components/Total';
-import Submitted from './components/Submitted';
-import Icon from '@material-ui/core/Icon';
-
-
-
-const PRICES = {
-HEROPRICE : 20,
-PORTRAITSPRICE : 20,
- FORMATSPRICE : 5,
- BASEPRICE : 30,
-PRICEPERFOOD : 5,
-PRICEPERDRINK : 5,
-}
+import React from 'react';
+import Form from './Form';
 
 function App() {
-  const[formpage, setFormpage] = useState(100)
-  const[liveprice, setLiveprice] = useState(0)
-  const[quote,setQuote] = useState({
-    food:0,
-    drink:0,
-    hero: false,
-    portraits: false,
-    deliveroo:false,
-    justeat:false,
-    ubereats:false,
-  })
-
-  
-
-
-  return (
-    <div className='app'>
-      {formpage === 100 && <Intro />}
-      {formpage === 200 && <HeroShot      quote={quote}  setQuote={setQuote}    />}
-      {formpage === 300 && <CreateScene   quote={quote}  setQuote={setQuote}   />}
-      {formpage === 400 && <Extras   quote={quote}  setQuote={setQuote}  />}
-      {formpage === 500 && <Formats   quote={quote}  setQuote={setQuote}  />}
-      {formpage === 600 && <Total   quote={quote}  setQuote={setQuote}  PRICES= {PRICES} setFormpage={setFormpage}/>}
-      {formpage === 700 && <Submitted />}
-
-      {formpage<600 && <button className='forward' onClick={forwardpressed}>Next step</button>}
-      {formpage > 100 && formpage<600 && <Pricecalc  quote={quote} PRICES= {PRICES}/>}
-      {formpage>100 && formpage<700 &&  <button className='back' onClick={backpressed}><Icon className='arrow'>arrow_back</Icon>go back</button>}
-
-    </div>
-  );
-
-
-
-function forwardpressed(){
-  setFormpage(formpage+100)
+    return (
+        <div className='app'>
+            {/* <Form /> */}
+        </div>
+    )
 }
 
-function backpressed(){
-  setFormpage(formpage-100)
-}
-
-} //app function
-
-
-
-
-
-
-
-export default App;
+export default App
---
+++ src/Form.js
@@ -0,0 +1,80 @@
+
+import './App.css';
+import './css/switch.css';
+import './css/table.css';
+import './css/extras.css';
+import './css/formats.css';
+import React, { useState } from 'react'
+import Intro from './components/Intro';
+import HeroShot from './components/HeroShot';
+import CreateScene from './components/CreateScene';
+import Extras from './components/Extras';
+import Pricecalc from './components/Pricecalc';
+import Formats from './components/Formats';
+import Total from './components/Total';
+import Submitted from './components/Submitted';
+import Icon from '@material-ui/core/Icon';
+
+
+
+const PRICES = {
+HEROPRICE : 20,
+PORTRAITSPRICE : 20,
+ FORMATSPRICE : 5,
+ BASEPRICE : 30,
+PRICEPERFOOD : 5,
+PRICEPERDRINK : 5,
+}
+
+function Form() {
+  const[formpage, setFormpage] = useState(100)
+  const[liveprice, setLiveprice] = useState(0)
+  const[quote,setQuote] = useState({
+    food:0,
+    drink:0,
+    hero: false,
+    portraits: false,
+    deliveroo:false,
+    justeat:false,
+    ubereats:false,
+  })
+
+  
+
+
+  return (
+    <div className='form'>
+      {formpage === 100 && <Intro />}
+      {formpage === 200 && <HeroShot      quote={quote}  setQuote={setQuote}    />}
+      {formpage === 300 && <CreateScene   quote={quote}  setQuote={setQuote}   />}
+      {formpage === 400 && <Extras   quote={quote}  setQuote={setQuote}  />}
+      {formpage === 500 && <Formats   quote={quote}  setQuote={setQuote}  />}
+      {formpage === 600 && <Total   quote={quote}  setQuote={setQuote}  PRICES= {PRICES} setFormpage={setFormpage}/>}
+      {formpage === 700 && <Submitted />}
+
+      {formpage<600 && <button className='forward' onClick={forwardpressed}>Next step</button>}
+      {formpage > 100 && formpage<600 && <Pricecalc  quote={quote} PRICES= {PRICES}/>}
+      {formpage>100 && formpage<700 &&  <button className='back' onClick={backpressed}><Icon className='arrow'>arrow_back</Icon>go back</button>}
+
+    </div>
+  );
+
+
+
+function forwardpressed(){
+  setFormpage(formpage+100)
+}
+
+function backpressed(){
+  setFormpage(formpage-100)
+}
+
+} //app function
+
+
+
+
+
+
+
+export default Form;
